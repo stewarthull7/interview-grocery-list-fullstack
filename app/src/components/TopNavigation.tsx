6347d084c656c56d799c15ac6b1668e29b307160
Stewart Hull <s.hull@cloudcart.com>
2024-07-03 09:05:05 +0000

    app: Added top navigation to default route layout

---
+++ app/src/components/TopNavigation.tsx
@@ -0,0 +1,122 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { useUser } from 'hooks/useUser';
+import { useLogout } from 'hooks/useAuth';
+
+function TopNavigation() {
+  const { logout } = useLogout()
+
+  const settings = [
+    {
+      name: 'Log out',
+      route: '/logout',
+      handler: logout,
+    }
+  ];
+
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [username, setUsername] = React.useState<null | string>(null)
+  const { data: user } = useUser()
+
+  React.useEffect(() => {
+    if (user && user.firstName) {
+      setUsername(`${user.firstName} ${user.lastName}`)
+    }
+  }, [user])
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            GOJI GROCERY
+          </Typography>
+
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            GOJI GROCERY
+          </Typography>
+
+          <Box sx={{ flexGrow: 0, marginLeft: 'auto' }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt={user && (username || user.email)} src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting.name} onClick={setting.handler}>
+                  <Typography textAlign="center">{setting.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default TopNavigation;
--- app/src/routes/_default.tsx
+++ app/src/routes/_default.tsx
@@ -1,5 +1,6 @@
 import { Outlet, createFileRoute } from '@tanstack/react-router'
 import Container from '@mui/material/Container'
+import TopNavigation from '@components/TopNavigation'
 
 export const Route = createFileRoute('/_default')({
   component: LayoutComponent
@@ -8,6 +9,7 @@ export const Route = createFileRoute('/_default')({
 function LayoutComponent() {
   return (
     <div>
+      <TopNavigation />
       <Container>
         <Outlet />
       </Container>
