a8c295d73cfb2b6f1492bec2340581f88d26ded4
Stewart Hull <s.hull@cloudcart.com>
2024-07-03 08:51:27 +0000

    app: Added theme provider and fixed body margin

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import App from 'App';
+
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        body {
+          margin: 0;
+        }
+      `,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
